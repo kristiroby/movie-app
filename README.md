d8f9a2ad7999511ebaf3030c3107bf039fb532c4
kristiroby@gmail.com <kristiroby@gmail.com>
2019-07-19 13:46:27 -0500

    fixed highlights

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # movie-app
+Used html, css, jQuery, AJAX and the movieDB APi.
+The API wasn't able to pull the type of information I originally planned, so I had to pivot a bit. I started by making sure I understood what I could call and how to access specific information. Then I made the drop down menus and search bar for the year. I had to figure out how to use the information the user selected to alter the API call.
--- app.js
+++ app.js
@@ -1,44 +1,65 @@
 $(() => {
-//drop downs
+    let $genre = "";
+    let $rating = "";
+    const $summary = $('<h5>').text('Click for summary')
+    //show drop down
+    $('#rating_head').on('click', (event) => {
+        $(event.target).find('.rating_category').show();
+    })
+    //get rating and hide drop downs
+    $('.rating_category').on('click', (event) => {
+        $(event.target).siblings().hide();
+        $rating = $(event.target).attr('id');
+    })
+    //show drop down
+    $('#genre_head').on('click', (event) => {
+        $(event.target).find('.genre_category').show();
+    })
+    //get genre and hide drop downs
+    $('.genre_category').on('click', (event) => {
+        $(event.target).siblings().hide();
+        $genre = $(event.target).attr('id');
+    })    
+    //create movie list
+    const $movies = $('<ul>');
+    $movies.addClass('movies')
+    $('#movieslist').append($movies);
+    //click movie list, clear list of movies, add year
+    $('form').on('submit', (event) => {
+        $movies.empty();
+        event.preventDefault();
+        const $year = ($('input[type="text"]').val());
+    //api call with user selected genre rating and year    
+    $.ajax({
+        url: "https://api.themoviedb.org/3/discover/movie?api_key=384794ce6f644a69db436d8258dc9352&language=en-US&sort_by=vote_average.desc&certification_country=US&include_adult=false&include_video=false&page=1&year=" + $year + "&certification=" + $rating + "&with_genres=" + $genre,
+    //show list of movies
+    }).then(
+        (data) => {
+            for (let i = 0; i < data.results.length; i++) {
+                $movies.prepend($summary)
+                const $title = $('<li>').text(data.results[i].title).addClass('title')
+                $movies.append($title)
+                $title.addClass("row")
+            
+            $title.on('click', (event) => {
+                let $index = $(event.target).index()
+                console.log($index)
+                let $plot = data.results[$index-1].overview
+                alert($plot)
+            })
+        }
+        },
+        (error) => {
+            console.log(error)
+        },
+    )
 
 
-$('.list').on('click', (event) => {
-    $(event.target).find('.category').show();
-}) 
-$('.category').on('click', (event) => {
-    $(event.target).parent().hide();
-    console.log($(event.target).attr('name'));
-}) 
-//$(event.currentTarget).parent().text) = 
-// const $movies = $('<ul>')
-// $('#movieslist').append($movies)
 
-//     $.ajax({
-//         url: "https://api.themoviedb.org/3/discover/movie?api_key=384794ce6f644a69db436d8258dc9352&language=en-US&sort_by=vote_average.desc&certification_country=US&include_adult=false&include_video=false&page=1",
-//         data: {
-          
-//         //     "agency": "NYPD",
-//         //     "borough": $selectedBorough,
-//         // eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODQ3OTRjZTZmNjQ0YTY5ZGI0MzZkODI1OGRjOTM1MiIsInN1YiI6IjVkMzA5MTg2MmY4ZDA5NDUyMmQyNTRiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6A12R5YKKFQe8v8CTLy0UaG5gnBJdBjL9_Ql_-4hK7Q
-//         }        
-//     }).then(
-//        (data) => {
-//         for (let i = 0; i < data.results.length; i++) {
-//             const $title = $('<li>').text(data.results[i].title).addClass('title')
-//                 $movies.append($title)
-        // console.log(data.results[i].title)
-        // }
 
-        
-        // },
-        // (error) => {
-        //     console.log(error)
-        // }
-    //     event.preventDefault();
-    // )
-    // })
-    // });
 });
+})
+// });
         // $('#title').html(data.title);
                 // $('#year').html(data.release_date);
                 //$('#rated').html(data.Rated);
